6b2a77721520d14e23ffd0ff7abce0bdd80f53e9
Jared Proske <jared.proske@hulu.com>
2018-11-12 10:55:30 -0800

    delete everything but typescript express server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,7 @@
-import errorHandler from "errorhandler";
+import express from "express";
 
-import app from "./app";
+const app = express();
 
-/**
- * Error Handler. Provides full stack - remove for production
- */
-app.use(errorHandler());
-
-/**
- * Start Express server.
- */
 const server = app.listen(app.get("port"), () => {
   console.log(
     "  App is running at http://localhost:%d in %s mode",
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,17 +1,5 @@
-import winston from "winston";
-import { Logger } from "winston";
-import { ENVIRONMENT } from "./secrets";
+import bunyan from 'bunyan';
 
-const logger = new (Logger)({
-    transports: [
-        new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
-        new (winston.transports.File)({ filename: "debug.log", level: "debug"})
-    ]
-});
-
-if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
-}
+const logger = bunyan.createLogger({ name: 'starter', level: 'debug' });
 
 export default logger;
-
